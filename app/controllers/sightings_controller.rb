3741574d6f8a834bd7a75ff7ecc4eee15a94d2b3
Warren Harper II <warrenharper2@att.net>
2020-06-11 23:39:58 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,6 @@
 class SightingsController < ApplicationController
 
-  def index
+    def index
       sightings = Sighting.all
       render json: SightingSerializer.new(sightings).to_serialized_json
     end
